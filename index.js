f966f7261894bcf748e2a85608e19e997f25da60
Chirag <chiragpandeybose@gmail.com>
2022-12-14 15:49:24 +0530

    asd

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let discoGridContainer = document.querySelector(".disco-grid-container");
 for (let i = 0; i < 504; i++) {
 	let box = document.createElement("div");
 	box.classList.add("box");
+    box.innerText = Math.floor(Math.random() * 500);
 	discoGridContainer.appendChild(box);
+    
 }
 let box = document.querySelectorAll(".box");
 discoGridContainer.addEventListener("mousemove", runEvent);
@@ -18,5 +20,6 @@ function runEvent() {
 			"," +
 			Math.random() * 250 +
 			")";
+		box[i].innerText = Math.floor(Math.random() * 500);
 	}
 }
